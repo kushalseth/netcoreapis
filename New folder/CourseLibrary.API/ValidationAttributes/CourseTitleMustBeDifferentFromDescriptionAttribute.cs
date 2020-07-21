e27df9ec602c137fd67142bdd4b93cb44ddb50b3
Kushal Seth <kushalseth1408@gmail.com>
2020-07-21 22:36:44 +0530

    coreapi

---
+++ New folder/CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using CourseLibrary.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+    }
+}
