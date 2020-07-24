dff6f9fef22d1f91b5970b3355defd7d450c67fe
Kushal Seth <kushalseth1408@gmail.com>
2020-07-24 23:31:58 +0530

    .net with core imtegrated

---
+++ webtemplate/webtemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webtemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
