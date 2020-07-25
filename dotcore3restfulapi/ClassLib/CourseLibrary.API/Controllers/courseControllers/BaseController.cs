f72e91574f58d949affa79acf48ebe30c0fcadc3
Kushal Seth <kushalseth1408@gmail.com>
2020-07-25 22:54:52 +0530

    added class module

---
+++ dotcore3restfulapi/ClassLib/CourseLibrary.API/Controllers/courseControllers/BaseController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using System.Security.Principal;
+using System.Security.Claims;
+
+namespace Hmsapp.Controllers
+{
+    public class BaseController : Controller
+    {
+
+        private ApplicationSignInManager _signInManager;
+        private ApplicationOperatorManager _userManager;
+
+        public BaseController()
+        {
+        }
+
+        public BaseController(ApplicationOperatorManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationOperatorManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationOperatorManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_userManager != null)
+                {
+                    _userManager.Dispose();
+                    _userManager = null;
+                }
+
+                if (_signInManager != null)
+                {
+                    _signInManager.Dispose();
+                    _signInManager = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
+    }
+
+    public static class UserExtended
+    {
+        public static string GetFullName(this IPrincipal user)
+        {
+            var claim = ((ClaimsIdentity)user.Identity).FindFirst(ClaimTypes.Name);
+            return claim == null ? null : claim.Value;
+        }
+        public static string GetAddress(this IPrincipal user)
+        {
+            var claim = ((ClaimsIdentity)user.Identity).FindFirst(ClaimTypes.StreetAddress);
+            return claim == null ? null : claim.Value;
+        }
+
+        public static string GetEmailAddress(this IPrincipal user)
+        {
+            var claim = ((ClaimsIdentity)user.Identity).FindFirst(ClaimTypes.Email);
+            return claim == null ? null : claim.Value;
+        }
+
+        public static string GetId(this IPrincipal user)
+        {
+            var claim = ((ClaimsIdentity)user.Identity).FindFirst(ClaimTypes.NameIdentifier);
+            return claim == null ? null : claim.Value;
+        }
+    }
+}
