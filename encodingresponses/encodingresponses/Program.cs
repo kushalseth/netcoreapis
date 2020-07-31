9ccc6f565b5e2c32d8b1e1f9981a3e1a3f02db32
Kushal Seth <kushalseth1408@gmail.com>
2020-07-31 23:03:34 +0530

    htmlencode

---
+++ encodingresponses/encodingresponses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace encodingresponses
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string having '&', '<', '>' or '\"' in it: ");
+            string myString = Console.ReadLine();
+
+            var jsonString = @"{""Name<"":""Rick"",""Company"":""West Wind"",
+                        ""Entered"":""2012-03-16&00:03:33.245-10:00""}";
+
+            dynamic json = JValue.Parse(jsonString);
+
+
+            // Encode the string.
+            string myEncodedString = HttpUtility.HtmlEncode(json);
+
+        }
+    }
+}
